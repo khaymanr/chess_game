6f5f31f00f70731267521308ccc2d794c059cd42
khaymanr <81813022+khaymanr@users.noreply.github.com>
2021-07-21 17:08:29 +0200

    Added the first structures for pieces, position and the board itself.

--- chess_game/src/application/App.java
+++ chess_game/src/application/App.java
@@ -1,7 +1,19 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import boardgame.Position;
+import boardgame.Piece;
+import boardgame.Board;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Board board = new Board(8, 8);
+
+        sc.close();
     }
 }
