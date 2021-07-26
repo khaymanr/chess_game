41a9da4c2eabe4459ef150bb9a09f1761b642b9b
khaymanr <81813022+khaymanr@users.noreply.github.com>
2021-07-26 14:01:59 +0200

    Added a visual indication of the possible moves for the rook.

--- chess_game/src/chess/ChessMatch.java
+++ chess_game/src/chess/ChessMatch.java
@@ -48,6 +48,12 @@ public class ChessMatch {
         placeNewPiece('d', 8, new King(board, Colour.BLACK));
     }
 
+    public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
